d98a29529a84d49950399124c27fa2983466b85c
Masahiro <masahiro19970823@yahoo.co.jp>
2026-02-02 21:16:05 +0900

    動作確認

--- function_app.py
+++ function_app.py
@@ -9,17 +9,17 @@ def showUser(req : func.HttpRequest) -> func.HttpResponse:
     defCred = DefaultAzureCredential()
 
     cosmos_url = 'https://cosmos20260202.documents.azure.com:443/'
-    cosmosClient = cosmos.CosmosClient(cosmos_url, defCred, '3')
+    #cosmosClient = cosmos.CosmosClient(cosmos_url, defCred, '3')
 
-    dbClient = cosmosClient.get_database_client('cosmosDb')
+    #dbClient = cosmosClient.get_database_client('cosmosDb')
 
-    dbClient.get_container_client('UserM')
+    #dbClient.get_container_client('UserM')
     
-    userList = dbClient.query_containers("SELECT * FROM c CountryID = 'Japan'")
+    #userList = dbClient.query_containers("SELECT * FROM c CountryID = 'Japan'")
 
-    html = '<h1>日本のユーザー一覧</h1>'
+    #html = '<h1>日本のユーザー一覧</h1>'
 
-    for user in userList:
-        html += user + '</br>'
+    #for user in userList:
+        #html += user + '</br>'
         
-    return func.HttpResponse(html, mimetype = 'text/html', status_code = 200)
+    return func.HttpResponse('<h1>日本のユーザー一覧</h1>', mimetype = 'text/html', status_code = 200)
