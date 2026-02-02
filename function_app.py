9e52e9870f686016459e33f749978affad0f1a61
Masahiro <masahiro19970823@yahoo.co.jp>
2026-02-02 21:58:10 +0900

    必須のファイルを作成

--- function_app.py
+++ function_app.py
@@ -6,9 +6,9 @@ app = func.FunctionApp()
 
 app.route('ShowUser', auth_level = func.AuthLevel.FUNCTION)
 def showUser(req : func.HttpRequest) -> func.HttpResponse:
-    defCred = DefaultAzureCredential()
+    #defCred = DefaultAzureCredential()
 
-    cosmos_url = 'https://cosmos20260202.documents.azure.com:443/'
+    #cosmos_url = 'https://cosmos20260202.documents.azure.com:443/'
     #cosmosClient = cosmos.CosmosClient(cosmos_url, defCred, '3')
 
     #dbClient = cosmosClient.get_database_client('cosmosDb')
